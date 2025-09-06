797cb94654b97ec317d5e84e8e45e62143d65d4f
AdMub <admub465@gmail.com>
2025-09-06 17:25:48 +0100

    model training updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src import logger
 from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-#from src.pipeline.stage_03_training import ModelTrainingPipeline
+from src.pipeline.stage_03_training import ModelTrainingPipeline
 #from src.pipeline.stage_04_evaluation import EvaluationPipeline
 
 
@@ -27,6 +27,19 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
 
 
 #logger.info("Welcome to our custom logging")
