76edf8939cdf532954d3628f190d935306d4cf7d
AdMub <admub465@gmail.com>
2025-09-06 15:47:21 +0100

    base model preparation updated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src import logger
 from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from src.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 #from src.pipeline.stage_03_training import ModelTrainingPipeline
 #from src.pipeline.stage_04_evaluation import EvaluationPipeline
 
@@ -15,6 +15,18 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 #logger.info("Welcome to our custom logging")
