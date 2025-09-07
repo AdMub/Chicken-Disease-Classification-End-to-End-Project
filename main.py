e0e0f508157e1966c91153e28aab0cc85b9d35e2
AdMub <admub465@gmail.com>
2025-09-07 10:10:13 +0100

    model and dvc successful

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src import logger
 from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.pipeline.stage_03_training import ModelTrainingPipeline
-#from src.pipeline.stage_04_evaluation import EvaluationPipeline
+from src.pipeline.stage_04_evaluation import EvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -40,6 +40,18 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 #logger.info("Welcome to our custom logging")
